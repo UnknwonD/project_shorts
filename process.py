b3458bac7e35bc8254228260cb3dc8eb8adbd86c
CoolD <daeho5000@naver.com>
2024-04-04 19:27:08 +0900

    Update process.py

--- process.py
+++ process.py
@@ -1,6 +1,6 @@
 import cv2
 
-def resize_video(input_video_path, output_video_path, width=256, height=256):
+def compress_video(input_video_path, output_video_path, width=256, height=256):
     # 영상 읽기
     video_capture = cv2.VideoCapture(input_video_path)
     
@@ -33,4 +33,4 @@ if __name__ == "__main__":
     input_video_path = 'data\\원천데이터\\2~5분\\SUMVID_SHORT_TRAIN_01\\0a966c42cd1dc53e58eb2f7a4b675e6c8eca48dc44e545a28b35e1c5a61df61e-└»╞⌐║Ω ┐╡╗≤╣░(2~5║╨) └σ╕Θ║░ ┴▀┐Σ╡╡ ┼┬▒╫-100176-20201124135655-001-001.mp4'
     output_video_path = 'processed/resized_video_3_gray.mp4'
 
-    resize_video(input_video_path, output_video_path)
+    compress_video(input_video_path, output_video_path)
