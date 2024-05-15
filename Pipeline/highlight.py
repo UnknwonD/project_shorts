a7f53bc65f96908d9a0c8eed70999bb06b8f3116
CoolD <daeho5000@naver.com>
2024-05-15 19:30:03 +0900

    PipeLine Test

--- Pipeline/highlight.py
+++ Pipeline/highlight.py
@@ -7,7 +7,7 @@ from process_video import preprocess_video_every_3_seconds
 from tensorflow.keras.models import load_model
 
 
-video_path = "../data/원천데이터/2~5분/SUMVID_SHORT_TRAIN_01/test.mp4"
+video_path = "Pipeline/test.mp4"
 
 
 def pipeline_video(video_path:str):
@@ -20,17 +20,19 @@ def pipeline_video(video_path:str):
     # 1) 오디오와 비디오 분리
     # 2) 분리된 비디오와 오디오 각각 전처리
     
-    audio = extract_audio(audio)
-    audio = preprocess_audio(video_path)
+    audio = extract_audio(video_path, './test.wav')
+    audio = preprocess_audio(audio)
     
 
     video = preprocess_video_every_3_seconds(video_path, (256, 256), 3)
 
+    print(len(video))
+    print(len(audio))
     ####### 3. 모델 불러오기
     # 1) 비디오 모델 load
     # 2) 오디오 모델 load
 
-    video_model = load_model("video_model.h5")
+    video_model = load_model("video_model.h5") # input으로 하나씩 넣어서 output을 append 하는 거로 수정할 것
     audio_model = load_model("audio_model_resnet.h5")
 
     ####### 4. 모델 추론
@@ -43,8 +45,16 @@ def pipeline_video(video_path:str):
     ####### 5. Ensemble
     ##
     ensemble_output = np.mean([video_output, audio_output], axis=0)
+    print("##################audio######################")
+    print(audio_output)
+    print("##################video######################")
+    print(video_output)
+    print("#############################################")
     final_predictions = np.argmax(ensemble_output, axis=1)
     
     ####### 6. Return
     return final_predictions
 
+test_data = pipeline_video(video_path)
+
+print(len(test_data))
